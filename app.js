e207eedae02d9f8b9f830157c8490f43f1743e96
Paul Copley <paul@northcoders.com>
2020-03-22 23:21:35 +0000

    remove auth endpoints

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ const express = require('express');
 const app = express();
 const { handleCustomErrors, handle500s } = require('./controllers/errors');
 const { revealSecrets } = require('./controllers/secrets');
-const { loginUser, validateUser } = require('./controllers/auth');
 
 app.use(express.json());
-
-// generate tokens
-app.post('/api/login', loginUser);
-
-// check tokens are valid
-app.use(validateUser);
 
 app.get('/api/secrets', revealSecrets);
 
