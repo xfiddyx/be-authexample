b35196ca08a16ee8df2a90873980f8af34357ca1
Paul Copley <paul@northcoders.com>
2020-03-22 23:23:59 +0000

    remove utils

--- utils/index.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-exports.formatUsers = rawUsers => {
-  return rawUsers.map(user => ({
-    ...user,
-    password: bcrypt.hashSync(user.password, 10)
-  }));
-};
