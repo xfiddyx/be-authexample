9ad950b0376f06376c94f5fcfac78477e9415c3e
Paul Copley <paul@northcoders.com>
2020-03-22 23:21:00 +0000

    remove username condition

--- controllers/secrets.js
+++ controllers/secrets.js
@@ -1,11 +1,9 @@
 const connection = require('../db/connection');
 
 exports.revealSecrets = (req, res, next) => {
-  const { username } = req.user;
   connection('secrets')
     .select('secrets.*', 'users.username')
     .join('users', 'users.user_id', '=', 'secrets.user_id')
-    .where('users.username', username)
     .then(secrets => {
       res.send({ secrets });
     });
